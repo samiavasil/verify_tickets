30eee8add136deb4a37c5f984720208633310ce3
samiavasil <samiavasil@gmail.com>
2020-02-13 00:28:43 +0200

    Update Json parser

--- src/tickets_service/AJRServer.cpp
+++ src/tickets_service/AJRServer.cpp
@@ -9,48 +9,37 @@ AJRServer::AJRServer(QObject *parent, const ServerConfigurator& config):
 
 }
 
+
+static QVariant getVal(QJsonObject& jsObj,  const char* field, QVariant::Type type = QVariant::String) {
+    QVariant val;
+    if(jsObj.contains(field) && jsObj[field].toVariant().canConvert(type)) {
+        val = jsObj[field].toVariant();
+        qDebug() << field << ": " << val.toString();
+    }
+    return val;
+}
+
 void AJRServer::ParseJsonInput(const QByteArray& buff) {
     QJsonParseError error;
     QJsonDocument doc = QJsonDocument::fromJson(buff, &error);
-
+    qDebug() << buff;
     if (doc.isNull()) {
         qDebug() <<"Json error>:" <<error.errorString();
     } else if(!doc.isArray()) {
         qDebug() << tr("Wrong json format: Expected to be arrea type");
     } else {
 
-        qDebug() << "Json Parser";//doc.toJson();
+        qDebug() << "Json Parser: " << doc.toJson();
         QJsonArray array = doc.array();
         for (int i = 0; i < array.count(); i++) {
             QJsonObject jsObj = array[i].toObject();
 
-            if(jsObj.contains("sale_id") && jsObj["sale_id"].isString()) {
-                qDebug() << "sale_id: " << jsObj["sale_id"].toString();
-            }
-
-            if(jsObj.contains("qr") && jsObj["qr"].isString()) {
-                qDebug() << "qr: " << jsObj["qr"].toString();
-            }
-
-            if(jsObj.contains("code")) {
-                int code = jsObj.value("code").toInt(-1);
-                if(code > 0) {
-                    qDebug() << "code: " << code;
-                }
-            }
-
-            if(jsObj.contains("name") && jsObj["name"].isString()) {
-                qDebug() << "name: " << jsObj["name"].toString();
-            }
-
-            if(jsObj.contains("qty")) {
-                if(jsObj.contains("qty")) {
-                    int qty = jsObj.value("qty").toInt(-1);
-                    if(qty > 0) {
-                        qDebug() << "qty: " << qty;
-                    }
-                }
-            }
+            QVariant val = getVal(jsObj, "mu_id",QVariant::Int);
+            val = getVal(jsObj, "sel_id",QVariant::Int);
+            val = getVal(jsObj, "qr",QVariant::String);
+            val = getVal(jsObj, "code",QVariant::String);
+            val = getVal(jsObj, "codename",QVariant::String);
+            val = getVal(jsObj, "qty",QVariant::Int);
         }
     }
 }
