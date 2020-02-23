1407d23524c5585ed332b4d7b279e78117845e07
samiavasil <samiavasil@gmail.com>
2020-02-24 01:07:58 +0200

    Test DBClient and CassTable
    TODO: Dellete all of these lnes. Used just to check some functionality.
    In final stage should be removed.

--- src/tickets_service/main.cpp
+++ src/tickets_service/main.cpp
@@ -4,6 +4,10 @@
 #include "Daemon.h"
 #include "QRServer.h"
 #include "TCPServer.h"
+#include "DBClient.h"
+
+#include "cassandra.h"
+#include "CassTable.h"
 
 int main(int argc, char *argv[])
 {
@@ -12,15 +16,22 @@ int main(int argc, char *argv[])
     Daemon::SetupUnixSignalHandlers();
     ServerConfigurator cfg1(QHostAddress::Any, 12345, false);
     ServerConfigurator cfg(QHostAddress::Any, 12346, false);
+    DBClient dbClient(&a);
+    QMap<QString, QString> columns({{"key1","int"},
+                                    {"key2","text"},});
+
+   CassTable t("test_keyspace", "site1", columns);
 
     QRServer qr_server(nullptr, cfg);
     cfg.setPort(12345);
     AJRServer ajr_server(nullptr, cfg1);
 
-
-    qDebug()<<QCoreApplication::libraryPaths();
+    qDebug() << QCoreApplication::libraryPaths();
 
     qDebug() << "Run " << a.applicationName();
 
+    dbClient.connectSession();
+
+    qDebug() << "Create Table: " << t.CreateTable(dbClient.session());
     return a.exec();
 }
